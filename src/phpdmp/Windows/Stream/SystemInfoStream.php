ecb43c22105c29d95178ffb5c6da6eaafc939ab4
Stephen Flannery <Stephen Flannery>
2015-11-27 15:42:30 -0500

    Started implementation of SystemInfoStream

--- src/phpdmp/Windows/Stream/SystemInfoStream.php
+++ src/phpdmp/Windows/Stream/SystemInfoStream.php
@@ -2,7 +2,50 @@
 
 namespace phpdmp\Windows\Stream;
 
+/*
+Contains processor and operating system information.
+    https://msdn.microsoft.com/en-us/library/windows/desktop/ms680396(v=vs.85).aspx
+*/
 class SystemInfoStream extends Stream
 {
+    public function __construct($streamData)
+    {
+        parent::__construct($streamData);
 
+        $unpackFormat = [
+            'vprocessorArchitecture',
+            'vprocessorLevel',
+            'vprocessorRevision',
+            'CnumberOfProcessors',
+            'CproductType',
+            'VmajorVersion',
+            'VminorVersion',
+            'VbuildNumber',
+            'VplatformId',
+            'VCSDVersionRVA',
+            'vsuiteMask',
+            'vreserved',
+            'V3vendorId',
+            'VversionInformation',
+            'VfeatureInformation',
+            'VamdExtendedCPUFeatures',
+        ];
+
+        foreach (unpack(implode('/', $unpackFormat), $streamData) as $variable => $value) {
+            $this->{$variable} = $value;
+        }
+    }
+
+    public function getMajorVersion()
+    {
+        return $this->majorVersion;
+    }
+    public function getMinorVersion()
+    {
+        return $this->minorVersion;
+    }
+    public function getBuildNumber()
+    {
+        return $this->buildNumber;
+    }
 }
